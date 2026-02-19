6739fa3464fc87eb9cec72b1d22095422844c88f
St. Bernadine <hdl.freelancing.business@gmail.com>
2026-02-19 16:13:54 +0800

    Fix: Switch to lighter gemini-1.5-flash-latest model to avoid 429 quota errors

--- tools/bump_version.js
+++ tools/bump_version.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const rootDir = '.'; // Running from project root
-const newVersion = '10.17';
+const newVersion = '10.18';
 
 // List of HTML files to scan (manually curated to avoid node_modules)
 const files = [
